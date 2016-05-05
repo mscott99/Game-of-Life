6024a5d12258dc4f874707805e37410dc4b7337d
Matthew <scom8642@gmail.com>
2016-05-05 12:00:09 -0400

    fixing title

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,7 +8,7 @@ private static Window window;
 
 public static void main(String[] args){
 System.out.println("Hello world!");
-window = new Window("this game is the game of life");
+window = new Window("Game of Life");
 }
 
 
